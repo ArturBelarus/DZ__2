4af7d298c41cef795c87337e6d4a85e613d51558
ArturBelarus <ruscuk09@gmail.com>
2022-10-28 22:13:54 +0300

    Решение

--- Tasks/Program.cs
+++ Tasks/Program.cs
@@ -1,1 +1,32 @@
-﻿
+﻿// Напишите программу, которая принимает на вход трехзначное число и на выходе показывает вторую цифру этого числа
+
+ int GiveSeconDigit(int number)
+ {
+     number = number / 10;
+     int secondDigit = number % 10;
+     return secondDigit;
+ }
+
+ bool CheckNumber(int number)
+ {
+     number = number / 100;
+     return number > 0 & number < 10;
+ }
+
+
+{
+    Console.WriteLine("Введите трех значное число");
+    int threeDigitNumber = Convert.ToInt32(Console.ReadLine());
+
+    bool result10 = CheckNumber(threeDigitNumber);
+    int secondDigit = GiveSeconDigit(threeDigitNumber);
+
+    if (result10)
+    {
+        Console.WriteLine($"{secondDigit}");
+    }
+    else
+    {
+        Console.WriteLine("Ошибка");
+    }
+}
